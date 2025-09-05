703469549439f96ca1b4fd44eab9a307ece7221f
Nithi-42 <nithi422005@gmail.com>
2025-09-05 09:29:06 +0400

    Add files via upload

--- views.py
+++ views.py
@@ -1,19 +1,24 @@
-from django.shortcuts import render
+
+from .models import Product, CartItem
+
+from django.shortcuts import render,redirect
 
 def home(request):
-    return render(request, 'mykart/index.html')
-def cart(request):
-    return render(request, 'mykart/cart.html')
+    return render(request, "mykart/index.html",)
+def view(request):
+    return render(request, 'mykart/view.html')
+def checkout(request):
+    return render(request,'mykart/checkout.html')
+
 def register(request):
     return render(request, 'mykart/register.html')
 def categories(request):
     return render(request, 'mykart/categories.html')
 
+def cart(request):
+	return render(request, 'mykart/cart.html')
+
+def skirts(request):
+     return render(request, 'mykart/skirts.html')
 
 
-
-
-
-
-
-# Create your views here.
